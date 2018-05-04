7df39b61e180f5c5bd14ed99a0dac622415b947a
Victor <547106693@qq.com>
2018-05-04 15:42:23 -0400

    rename

---
+++ examples/tweets/run_dssm.sh
@@ -0,0 +1,10 @@
+cd ../../
+
+currpath=`pwd`
+# train the model
+python matchzoo/main.py --phase train --model_file ${currpath}/examples/tweets/config/dssm_trec.config
+
+
+# predict with the model
+
+python matchzoo/main.py --phase predict --model_file ${currpath}/examples/tweets/config/dssm_trec.config
