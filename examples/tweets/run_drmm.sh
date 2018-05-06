484d52304b7646a9392146c7bd2c98bcc0457be8
Victor0118 <547106693@qq.com>
2018-05-06 16:20:12 -0400

    rename3

--- examples/tweets/run_drmm.sh
+++ examples/tweets/run_drmm.sh
@@ -2,9 +2,9 @@ cd ../../
 
 currpath=`pwd`
 # train the model
-python matchzoo/main.py --phase train --model_file ${currpath}/examples/trec/config/drmm_trec.config
+python matchzoo/main.py --phase train --model_file ${currpath}/examples/tweets/config/drmm_tweets.config
 
 
 # predict with the model
 
-python matchzoo/main.py --phase predict --model_file ${currpath}/examples/trec/config/drmm_trec.config
+python matchzoo/main.py --phase predict --model_file ${currpath}/examples/tweets/config/drmm_tweets.config
--- examples/tweets/run_dssm.sh
+++ examples/tweets/run_dssm.sh
@@ -2,9 +2,9 @@ cd ../../
 
 currpath=`pwd`
 # train the model
-python matchzoo/main.py --phase train --model_file ${currpath}/examples/tweets/config/dssm_trec.config
+python matchzoo/main.py --phase train --model_file ${currpath}/examples/tweets/config/dssm_tweets.config
 
 
 # predict with the model
 
-python matchzoo/main.py --phase predict --model_file ${currpath}/examples/tweets/config/dssm_trec.config
+python matchzoo/main.py --phase predict --model_file ${currpath}/examples/tweets/config/dssm_tweets.config
